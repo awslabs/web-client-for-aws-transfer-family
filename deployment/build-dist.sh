a2d1102a293d8fda1927aa994fe92084f0999f0a
Russ Sivak <Russ.Sivak@moodys.com>
2021-10-26 11:13:01 -0400

    bring build-dist help in line with the readme

--- deployment/build-dist.sh
+++ deployment/build-dist.sh
@@ -9,7 +9,7 @@
 # Parameters:
 #  - source-bucket-base-name: Name for the S3 bucket location where the template will source the Lambda
 #    code from. The template will append '-[region_name]' to this bucket name.
-#    For example: ./build-dist.sh solutions web-client-for-aws-transfer v1.0.0
+#    For example: ./build-dist.sh solutions web-client-for-aws-transfer-family v1.0.0
 #    The template will then expect the lambda source code to be located in the solutions-[region_name] bucket
 #
 #  - solution-name: name of the solution for consistency
@@ -19,7 +19,8 @@
 # Check to see if input has been provided:
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     echo "Please provide the base source bucket name, trademark approved solution name and version where the lambda code will eventually reside."
-    echo "For example: ./build-dist.sh solutions web-client-for-aws-transfer v1.0.0"
+    echo "For example: ./build-dist.sh solutions web-client-for-aws-transfer-family v1.0.0
+"
     exit 1
 fi
 
